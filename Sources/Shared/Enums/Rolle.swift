6919f5274d1b7dc90d695cb4d7bd84cff8af8fc6
Erik <erikli.schnell@gmail.com>
2024-06-18 23:55:50 +0200

    Rolle is comparable

--- Sources/Shared/Enums/Rolle.swift
+++ Sources/Shared/Enums/Rolle.swift
@@ -13,3 +13,21 @@ public enum Rolle: Codable {
     case betreuung
     case student
 }
+
+extension Rolle: Comparable {
+    public static func < (lhs: Rolle, rhs: Rolle) -> Bool {
+        func priority(of role: Rolle) -> Int {
+            switch role {
+            case .master:
+                return 1
+            case .admin:
+                return 2
+            case .betreuung:
+                return 3
+            case .student:
+                return 4
+            }
+        }
+        return priority(of: lhs) < priority(of: rhs)
+    }
+}
